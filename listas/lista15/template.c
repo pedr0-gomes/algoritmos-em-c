2a81f9ca3c4a168aa1e1a6a6d8f2a46856f5fc4d
pedr0-gomes <gomes.pedro@aluno.ufca.edu.br>
2026-03-03 09:38:57 -0300

    docs(lista15): add template base para novos exercícios

---
+++ listas/lista15/template.c
@@ -0,0 +1,39 @@
+/*
+Lista: 15
+Exercício: XX
+Enunciado: ...
+
+Entradas: ...
+Saídas: ...
+
+Autor: Pedro Gomes
+Data: DD/MM/AAAA
+
+-----------------------------------------
+PSEUDOCODE (OPCIONAL)
+-----------------------------------------
+
+// PSEUDOCODE-START
+
+// PSEUDOCODE-END
+*/
+
+#include <stdio.h>
+#include "bst.h"
+
+/* protótipo da função específica deste exercício */
+// tipo nome_da_funcao(RegInt* p);
+
+int main()
+{
+    RegInt* raiz = NULL;
+
+    /* insira os valores da árvore aqui */
+
+    /* chame a função do exercício aqui */
+
+    bst_liberar_arvore(raiz);
+    return 0;
+}
+
+/* implemente a função específica deste exercício aqui */
